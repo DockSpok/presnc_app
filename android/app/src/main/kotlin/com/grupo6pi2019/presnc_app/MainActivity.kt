89eac4c48867a2085a7b329eb78639d990f197b1
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2019-11-04 03:40:28 -0300

    commit inicial estrutura

---
+++ android/app/src/main/kotlin/com/grupo6pi2019/presnc_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.grupo6pi2019.presnc_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
